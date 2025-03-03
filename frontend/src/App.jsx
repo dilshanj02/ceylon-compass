30e1c9120a1ff4ad019dfcd3641c4b901cecdd27
dilshanj02 <stud.dilshanj@gmail.com>
2025-03-03 21:43:29 +0530

    feat: add footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import PlanTrip from "./pages/PlanTrip";
 
 function App() {
   return (
     <Router>
-      <Header />
-      <div className="mt-20 p-6">
-        <Routes>
-          <Route
-            path="/"
-            element={<h1 className="text-4xl font-bold p-6">Home Page</h1>}
-          />
-          <Route path="/plan" element={<PlanTrip />} />
-          <Route
-            path="/trips"
-            element={<h1 className="text-4xl font-bold p-6">Trips</h1>}
-          />
-          <Route
-            path="/community"
-            element={<h1 className="text-4xl font-bold p-6">Community</h1>}
-          />
-          <Route
-            path="/login"
-            element={<h1 className="text-4xl font-bold p-6">Login Page</h1>}
-          />
-        </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Header />
+
+        <div className="flex-grow mt-20 p-6">
+          <Routes>
+            <Route
+              path="/"
+              element={<h1 className="text-4xl font-bold p-6">Home Page</h1>}
+            />
+            <Route path="/plan" element={<PlanTrip />} />
+            <Route
+              path="/trips"
+              element={<h1 className="text-4xl font-bold p-6">Trips</h1>}
+            />
+            <Route
+              path="/community"
+              element={<h1 className="text-4xl font-bold p-6">Community</h1>}
+            />
+            <Route
+              path="/login"
+              element={<h1 className="text-4xl font-bold p-6">Login Page</h1>}
+            />
+          </Routes>
+        </div>
+
+        <Footer />
       </div>
     </Router>
   );
